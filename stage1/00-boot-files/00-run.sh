29ef41a31b601739e6497dd8b924d5b898e62ac7
Zdzislaw Sedek <zdzislaw.sedek@gmail.com>
2020-01-31 06:00:36 +0100

    Distribution personalization, installing connman, enable by default ethernet over usb and adding lcd support

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -2,3 +2,21 @@
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+
+enable_usb_ethernet_in_cmdline(){
+
+}
+
+enable_usb_ethernet_in_config_file()
+{
+cat << EOF >> "${ROOTFS_DIR}/boot/config.txt"
+
+# Ethernet over USB
+dtoverlay=dwc2
+EOF
+}
+
+if [ "${ETHERNET_USB}" == "1" ]; then
+	enable_usb_ethernet_in_config_file
+	enable_usb_ethernet_in_cmdline
+fi
