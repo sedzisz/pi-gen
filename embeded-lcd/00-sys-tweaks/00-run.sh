6fe7bf8d97df6e9e710675d9ff4a87a944417909
Zdzislaw Sedek <zdzislaw.sedek@gmail.com>
2020-01-31 12:37:24 +0100

    Update

--- embeded-lcd/00-sys-tweaks/00-run.sh
+++ embeded-lcd/00-sys-tweaks/00-run.sh
@@ -4,7 +4,7 @@ enable_fbcp()
 {
 echo "Installing application for cloning framebuffer in to LCD."
 install -m 644 files/fbcp "${ROOTFS_DIR}/bin/"
-cat >> ${ROOTFS_DIR}/etc/modules <<EOF 
+cat >> "${ROOTFS_DIR}/etc/modules" <<EOF 
 #Enable LCD on boot time.
 fbtft_device
 EOF
@@ -13,7 +13,7 @@ EOF
 enable_lcd_waveshare_st7735s()
 {
 echo "Enable configuration for LCD Waveshare ST7735S"
-cat  >> ${ROOTFS_DIR}/etc/modprobe.d/options.conf <<EOF
+cat  >> "${ROOTFS_DIR}/etc/modprobe.d/options.conf" <<EOF
 options fbtft_device name=adafruit18_green gpios=reset:27,dc:25,cs:8,led:24 speed=40000000 bgr=1 fps=60 custom=1 height=128 width=128 rotate=90
 EOF
 }
--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -7,7 +7,7 @@ install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
 
 enable_usb_ethernet_in_config_file()
 {
-cat >> ${ROOTFS_DIR}/boot/config.txt <<EOF
+cat >> "${ROOTFS_DIR}/boot/config.txt" <<EOF
 
 # Ethernet over USB
 dtoverlay=dwc2
@@ -16,8 +16,8 @@ EOF
 
 main()
 {
-	echo "Enabling option: 'dwc2'."
-	enable_usb_ethernet_in_config_file;
+	log "Enabling option: 'dwc2'."
+	enable_usb_ethernet_in_config_file
 }
 
 # if [ "${ETHERNET_USB}" = "1" ]; then
